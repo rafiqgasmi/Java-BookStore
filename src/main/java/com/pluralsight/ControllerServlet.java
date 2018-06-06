c282f8bd847eef59385d0972cde7444a5245b03a
rafiqgasmi <39992727+rafiqgasmi@users.noreply.github.com>
2018-06-06 17:34:58 +0100

    Adding Databse Admin Feautre

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -129,9 +129,9 @@ public class ControllerServlet extends HttpServlet {
 
 	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int id = Integer.parseInt(request.getParameter("id"));
-		Book book = bookDAO.getBook(id);
+		Book existingBook = bookDAO.getBook(id);
 		RequestDispatcher requestdispatcher = request.getRequestDispatcher("BookForm.jsp");
-		request.setAttribute("book", book);
+		request.setAttribute("book", existingBook);
 		requestdispatcher.forward(request,response);
 	}
 
