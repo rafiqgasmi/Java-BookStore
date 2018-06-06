c7067bef6a5888967b9dc8f1dc74e799de548f31
rafiqgasmi <39992727+rafiqgasmi@users.noreply.github.com>
2018-06-06 16:23:13 +0100

    Deleting book from a database

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -92,9 +92,9 @@ public class BookDAO {
     }
 
     public void deleteBook(int id){
-        String sql = "DELETE FROM book WHERE id =?";
+        String SQL = "DELETE FROM book WHERE id =?";
         try {
-            PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+            PreparedStatement statement = jdbcConnection.prepareStatement(SQL);
             statement.setInt(1, id);
             statement.executeUpdate();
             statement.close();
